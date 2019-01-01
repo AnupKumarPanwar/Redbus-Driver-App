2182222d19356182fd0723cbabe6041fc17cce16
Anup Kumar Panwar <1anuppanwar@gmail.com>
2019-01-01 18:12:52 +0530

    intent clear top

--- app/src/main/java/com/gotobus/OTPActivity.java
+++ app/src/main/java/com/gotobus/OTPActivity.java
@@ -73,6 +73,7 @@ public class OTPActivity extends AppCompatActivity {
                                         editor.putString("access_token", result.getJSONObject("data").get("access_token").toString());
                                         editor.commit();
                                         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                                         startActivity(intent);
                                         finish();
                                     } else {
